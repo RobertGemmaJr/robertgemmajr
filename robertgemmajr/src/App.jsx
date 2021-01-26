2697cfc277a6511120884daa74e620333aa1e5bb
RobertGemmaJr <38439940+RobertGemmaJr@users.noreply.github.com>
2021-01-25 23:41:19 -0500

    Added reactt-icons and setup footer

--- robertgemmajr/src/App.jsx
+++ robertgemmajr/src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer"
 export default function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1> Hello, World </h1>
       <Footer />
     </div>
   );
--- robertgemmajr/src/components/Footer.jsx
+++ robertgemmajr/src/components/Footer.jsx
@@ -1,6 +1,38 @@
 import React from "react";
+import styled from "styled-components"
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
 
-const year = new Date().getFullYear();
+const Icons = styled.div`
+  width: 50%;
+  margin: 15px auto;
+  display: flex;
+  justify-content: space-between;
+
+  a {
+    color: pink;
+    :hover {
+      color: black;
+    }
+  }
+`;
+
+const Copyright = styled.p`
+  text-align: center;
+  font-size: 1.2em;
+`;
+
 export default function Footer() {
-    return(<h1>Footer: {year}</h1>);
+  const year = new Date().getFullYear();
+
+  return(
+    <div>
+      <Icons >
+        <a href="https://www.linkedin.com/in/robert-gemma-jr/"><FaLinkedin size={30}/></a>
+        <a href="https://github.com/RobertGemmaJr"><FaGithub size={30}/></a>
+        <a href="https://twitter.com/RobertGemmaJr"><FaTwitter size={30}/></a>
+        <a href="https://www.instagram.com/robertgemmajr/?hl=en"><FaInstagram size={30}/></a>
+      </Icons>
+      <Copyright>Copyright {year} © Robert Gemma, Jr.</Copyright>
+    </div>
+  );
 }
