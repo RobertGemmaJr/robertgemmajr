d4264617df7d8db3c4c1b6dfd6be3c12f607620b
RobertGemmaJr <38439940+RobertGemmaJr@users.noreply.github.com>
2021-02-09 16:15:12 -0500

    Added temp file for themes

--- robertgemmajr/src/App.jsx
+++ robertgemmajr/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import styled from "styled-components";
 
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
@@ -7,20 +8,6 @@ import Contact from "./components/pages/Contact";
 import Error from "./components/pages/Error";
 import Portfolio from "./components/pages/Portfolio"
 import Navigation from "./components/Navigation";
-
-// body {
-//   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-//     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-//     sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-// }
-
-// code {
-//   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-//     monospace;
-// }
-
 
 export default function App() {
   return (
---
+++ robertgemmajr/src/tempThemes.js
@@ -0,0 +1,78 @@
+// Theme Colors
+  // Adobe Color - Choose your own color
+  // colorhunt.co - Already done color pallettes
+  //https://visme.co/blog/website-color-schemes/
+const ContemporaryBold = styled.div`
+  color: #1A1A1D;
+  color: #4E4E50;
+  color: #6F2232;
+  color: #950740;
+  color: #C3073F;
+`;
+
+const StrikingSimple = styled.div`
+  color: #0B0C10;
+  color: #1F2833;
+  color: #C5C6C7;
+  color: #66FCF1;
+  color: #45A29E;
+`;
+
+const ModenMinimalist = styled.div`
+  color: #19181A;
+  color: #479761;
+  color: #CEBC81;
+  color: #A16E83;
+  color: #B19F9E;
+`;
+
+const InnovativeAudacious = styled.div`
+  color: #272727;
+  color: #747474;
+  color: #FF652F;
+  color: #FFE400;
+  color: #14A76C;
+`;
+
+const MinimalWarm = styled.div`
+  color: #EAE7DC;
+  color: #D8C3A5;
+  color: #8E8D8A;
+  color: #E98074;
+  color: #E85A4F;
+`;
+
+// Theme Fonts
+const Given = styled.div`
+  body {
+    font-family: "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
+
+const LoraRoboto = styled.div`
+  h1 {
+    font-family: 'Lora', serif;
+  }
+  p {
+    font-family: 'Roboto', sans-serif;
+  }
+`;
+
+const Mine = styled.div`
+  h1 {
+    font-family: 'Ubuntu Condensed', sans-serif;
+  }
+  p {
+    font-family: 'Lora', serif;
+  }
+`;
